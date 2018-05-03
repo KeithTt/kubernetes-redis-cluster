eb68222121948099a5ddb3719952f9ba113e67b2
KeithTt <shengyongp@oupeng.com>
2018-05-03 17:50:00 +0800

    update

---
+++ deployment-setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+kubectl create configmap redis-conf --from-file=redis.conf
+
+kubectl create -f persistentvolume
+
+kubectl create -f persistentvolumeclaims
+
+kubectl create -f deployment
+
+kubectl create -f deployment-services
